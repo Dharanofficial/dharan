1083742d9ecce2d064ae9d5bc7ecebdad375a269
sasidharan <ksasibca@gmail.com>
2025-03-10 12:44:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+import './App.css';
+import Sasi from './menu';
+function App() {
+  return (
+    <div className="App">
+      <Sasi/>       
+    </div>
+  );
+}
+export default App;
---
+++ src/resume.js
@@ -0,0 +1,14 @@
+import React from "react";
+function resume(){
+    return(
+        <div>
+            <center>
+                <h1>Resume
+                    love
+                    MySqllove
+                </h1>
+            </center>
+        </div>
+    )
+}
+export default resume
